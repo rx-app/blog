da481d24692ff40f7d0b645de9fac5bbf3c3ef32
dingming <dingming@hz-health.cn>
2022-01-14 09:52:51 +0800

    安装插件

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
-module.exports = {
-  reactStrictMode: true,
+// module.exports = {
+//   reactStrictMode: true,
+// }
+
+
+const withCss = require('@zeit/next-css')
+
+if(typeof require !== 'undefined'){
+    require.extensions['.css']=file=>{}
 }
+
+module.exports = withCss({
+  reactStrictMode: true,
+})
